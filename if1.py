ff21f83efb7ac5dcc68b4c7e1b1945c2b8ed7a94
zelpyo <yo.ultra.0804@gmail.com>
2025-03-26 13:08:19 +0000

    for文の練習

---
+++ if1.py
@@ -0,0 +1,4 @@
+scor = 70
+if scor >= 80:
+    print("やったね！")
+    print("次もこの調子だ")
---
+++ turtle3.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+col = ["orangi","limegreen","gold","plum","tomato"]
+for i in range(5):
+    color(col[i])
+    forward(200)
+    left(144)
+done()
