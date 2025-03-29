8b04d500bf8da9efc108b38a92ce843d12ac1ffb
zelpyo <yo.ultra.0804@gmail.com>
2025-03-29 06:55:40 +0000

    orizinal2.py

--- original2.py
+++ original2.py
@@ -1,22 +1,49 @@
 import random 
-h = quiz = [input"('''
-ヒント❶：赤い食べ物
-ヒント❷：丸い果物
-ヒント❸：最初に「り」がつく
-この食べ物何でしょう
+h = float
+quizList = ["ヒント❶：赤い食べ物"+"\n"
+           +"ヒント❷：丸い果物"+"\n"
+           +"ヒント❸：最初に「り」がつく"+"\n"
+           +"\n"
+           +"この食べ物何でしょう"+"\n"+
+           "選択肢1：いちご"+"\n"
+           +"選択肢2：トマト"+"\n"
+           +"選択肢3：りんご"+"\n"
+           +">",
+'''
+ヒント❶:鳥
+ヒント❷：黒い
+ヒント❸：「カーカー」と鳴く
+この生き物何でしょう
+選択肢1：ハト
+選択肢2：カラス
+選択肢3：すずめ
+>'''
+]
 
-選択肢1：いちご
-選択肢2：トマト
-選択肢3：りんご
-> ''')","h = ('''
-ヒント1: 鳥です。
-ヒント2: 黒いです。
-ヒント3: 「カーカー」と鳴きます。
->''')"]
-print(random.choice(quiz))
-if h == 3:
-    print("正解！")
-    print("おめでとう！")
+choiced = random.randint(0,1)
+#print("choiced is ",choiced)
+h = int(input(quizList[choiced]))
+
+if h.isdigit():
+    printh = int(input(quizList[choiced]))
+
 else:
-    print("残念")
-    print("正解は❸　の「りんご」でした")
+    print("数値じゃないよ")
+#print("h is '",h,"'")
+
+if choiced == 0:
+    if h == 3:
+        print("正解！")
+        print("おめでとう！")
+    else:
+        print("残念")
+        print("正解は❸　の「りんご」でした")
+
+
+if choiced == 1:
+    if h == 2:
+        print("正解！")
+        print("おめでとう！")
+    else:
+        print("残念")
+        print("正解は❷　の「カラス」でした")
